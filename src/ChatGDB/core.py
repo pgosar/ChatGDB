75d3cbb5a433791b1f57700ff9ef505888d64a67
pgosar <pgosarsteam@gmail.com>
2023-03-31 16:56:10 -0500

    change license and lint code

--- src/ChatGDB/core.py
+++ src/ChatGDB/core.py
@@ -3,10 +3,13 @@ import json
 from urllib.error import HTTPError, URLError
 from urllib.request import Request, urlopen
 from pathlib import Path
+
+
 key = []
 secret = ""
 path = Path(__file__).parent / "../../.env"
-# obtain appropriate api key from env file but without any dependencies :)
+
+# obtain appropriate api key from env file
 with path.open() as f:
     key = [line.strip() for line in f]
 for k in key:
@@ -21,23 +24,37 @@ def make_request(url, headers=None, data=None):
         with urlopen(request, timeout=10) as response:
             return response.read(), response
     except HTTPError as error:
-         print(error.status, error.reason)
+        print(error.status, error.reason)
     except URLError as error:
-         print(error.reason)
+        print(error.reason)
     except TimeoutError:
-         print("Request timed out")
+        print("Request timed out")
+
+
 
 class ChatGDB(gdb.Command):
     def __init__(self):
         super(ChatGDB, self).__init__("chat_gdb", gdb.COMMAND_DATA)
 
+    # creates api request on command invocation
     def invoke(self, arg, from_tty):
-        promptEngineering = "Give me GDB commands with no explanation. Do NOT write any english above or below the command. Only give me the command as text. Here is my question: "
+        promptEngineering = "Give me GDB commands with no explanation. Do NOT write any \
+                english above or below the command. Only give me the command as text. \
+                Here is my question: "
+
         url = "https://api.openai.com/v1/chat/completions"
-        headers = {"Authorization": "Bearer " + secret, "Content-Type": "application/json"}
-        data = {"model": "gpt-3.5-turbo", "messages":[{"role": "user", "content": promptEngineering + arg}] }
-        body, response = make_request(url, headers, data=bytes(json.dumps(data), encoding="utf-8"))
+        headers = {
+            "Authorization": "Bearer " + secret,
+            "Content-Type": "application/json"}
+        data = {"model": "gpt-3.5-turbo",
+                "messages": [{"role": "user",
+                              "content": promptEngineering + arg}]}
+
+        body, response = make_request(
+            url, headers, data=bytes(
+                json.dumps(data), encoding="utf-8"))
         body = json.loads(body)
         gdb.execute(body['choices'][0]['message']['content'])
+
 
 ChatGDB()
