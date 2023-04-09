65da5c8558c3ab96ae591953e8a89c1ab26e52e9
pgosar <pgosarsteam@gmail.com>
2023-04-08 19:05:40 -0500

    add version command

--- chatgdb/cli.py
+++ chatgdb/cli.py
@@ -1,6 +1,8 @@
 import argparse
 from os.path import abspath, dirname
 from inspect import getfile, currentframe
+from urllib.request import Request, urlopen
+import json
 
 
 def set_key(key):
@@ -8,6 +10,12 @@ def set_key(key):
     path = dirname(abspath(getfile(currentframe()))) + "/.secret.txt"
     with open(path, "w") as f:
         f.write("OPENAI_KEY=\"" + key + "\"")
+
+
+def version():
+    """Return version information"""
+    with urlopen(Request("https://pypi.org/pypi/chatgdb/json"), timeout=10) as f:
+        return json.load(f)["info"]["version"]
 
 
 def main():
@@ -19,6 +27,12 @@ def main():
         "--key",
         type=str,
         help="Provide an api key for ChatGDB")
+    parser.add_argument(
+        '-v',
+        "--version",
+        action="version",
+        version="%(prog)s " + version(),
+        help="Print the version of ChatGDB")
 
     args = parser.parse_args()
     if args.key:
